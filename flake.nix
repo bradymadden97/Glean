738914979fd3bf5e05b0a89f0b3c19b1acb597cc
Brady Madden <bradymadden97@gmail.com>
2025-10-18 14:05:16 -0400

    debugging

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Glean custom overlay flake for aarch64-darwin";
+  description = "Glean debug overlay flake for aarch64-darwin";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
@@ -10,46 +10,7 @@
         let
           hlib = final.haskell.lib;
           overlayHS = hfinal: hprev: {
-            _ = builtins.trace (builtins.typeOf hprev.glean) null;
-            glean =
-              (hlib.overrideCabal (old: {
-                libraryHaskellDepends = (old.libraryHaskellDepends or []) ++ [ hfinal.hinotify ];
-                jailbreak = true;
-              }) (hprev.glean {}))
-              .overrideAttrs (oldAttrs: {
-                postPatch = ''
-                  ${oldAttrs.postPatch or ""}
-                  rm Setup.hs
-                '';
-              });
-            fb-util =
-              (hlib.dontCheck (hlib.unmarkBroken (hlib.doJailbreak hprev.fb-util))).overrideAttrs
-                (oldAttrs: {
-                  env = (prev.env or { }) // {
-                    NIX_CFLAGS_COMPILE = "${
-                      oldAttrs.env.NIX_CFLAGS_COMPILE or ""
-                    } -DGFLAGS_IS_A_DLL=0 -DGLOG_USE_GLOG_EXPORT -DGLOG_USE_GFLAGS -lglog";
-                  };
-                  postPatch = ''
-                    ${oldAttrs.postPatch or ""}
-                    substituteInPlace ./fb-util.cabal --replace-fail "libglog," "" --replace-fail "g++" "c++"
-                    substituteInPlace ./Util/AsanAlloc.cpp --replace-fail "#include <malloc.h>" "#include <stdlib.h>"
-                  '';
-                });
-            thrift-compiler = hlib.doJailbreak hprev.thrift-compiler;
-            thrift-lib = hlib.dontCheck (hlib.doJailbreak hprev.thrift-lib);
-            thrift-http = hlib.dontCheck (hlib.doJailbreak hprev.thrift-http);
-            thrift-haxl = hlib.dontCheck (hlib.doJailbreak hprev.thrift-haxl);
-            fb-stubs = hlib.unmarkBroken (hlib.doJailbreak hprev.fb-stubs);
-            folly-clib = hprev.callCabal2nixWithOptions "folly-clib" (final.fetchzip {
-              url = "mirror://hackage/folly-clib-20250713.1537/folly-clib-20250713.1537.tar.gz";
-              sha256 = "sha256-pmiJ9TDn/TGs/DZwdkk0hl9rCyBjIeQfNDg6mTEMH40=";
-            }) "-f-bundled-folly" { libfolly = final.folly; };
-            haxl = (hlib.unmarkBroken hprev.haxl).overrideAttrs (oldAttrs: {
-              postPatch = "rm Setup.hs";
-            });
-            haskell-names = hlib.unmarkBroken (hlib.dontCheck (hlib.doJailbreak hprev.haskell-names));
-            mangle = hlib.doJailbreak hprev.mangle;
+            _trace = builtins.trace (builtins.typeOf hprev.glean) null;
           };
         in
         {
@@ -64,12 +25,7 @@
     in
     {
       overlays.default = overlay;
-
-      # Expose the package set for aarch64-darwin only
-      packages.aarch64-darwin.glean = (import nixpkgs {
-        system = "aarch64-darwin";
-        overlays = [ overlay ];
-      }).haskell.packages.ghc9103.glean;
+      # No packages output needed for this test
     };
 }
 
